45cbaed46d19eabaf82d53618aa491c71da27331
shaileshsonareg <156632889+shaileshsonareg@users.noreply.github.com>
2025-11-08 11:07:34 +0000

    multi threading

---
+++ Multithreading.java
@@ -0,0 +1,39 @@
+class Hi extends Thread {
+
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000); //1000 millisecond => 1 sec
+            } catch( Exception e) {}
+        }
+    }
+
+}
+
+class Huku extends Thread {
+
+    public void run() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Huku");
+            try {
+                Thread.sleep(1000); //1000 millisecond => 1 sec
+            } catch( Exception e) {}
+        }
+    }
+
+}
+
+class Main {
+    static public void main(String args[]) {
+        Hi hiobj = new Hi();
+        Huku hukuobj = new Huku();
+
+        hiobj.start();
+        hukuobj.start();
+
+        // hiobj.run();
+        // hukuobj.run();
+    }
+
+}
